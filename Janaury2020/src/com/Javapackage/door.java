bab3c7a033d08c277296287b1ccc58ab64e2dc4c
shravan2020hhh <60684220+shravan2020hhh@users.noreply.github.com>
2020-02-07 13:44:01 +0530

    hgklo

---
+++ Janaury2020/src/com/Javapackage/door.java
@@ -0,0 +1,16 @@
+package com.Javapackage;
+
+public class door 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hhhhhhh");
+		System.out.println("ggggggg");
+		System.out.println("kkkkkkk");
+		System.out.println("lllllll");
+		System.out.println("ooooooo");
+		
+		
+		
+	}
+}
