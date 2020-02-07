7a983aa460128090698c645e90c67a7290f30532
shravan2020hhh <60684220+shravan2020hhh@users.noreply.github.com>
2020-02-07 13:36:02 +0530

    child

---
+++ Janaury2020/src/com/Javapackage/Absnew.java
@@ -0,0 +1,16 @@
+package com.Javapackage;
+
+public abstract class Absnew 
+{
+	public void m1()
+	{
+		System.out.println("Iam m1 concrete method in class Absnew");
+	}
+	
+	public abstract void m2();
+	
+	
+	
+	
+	
+}
---
+++ Janaury2020/src/com/Javapackage/Child.java
@@ -0,0 +1,12 @@
+package com.Javapackage;
+
+public class Child
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("gfgfg");
+
+	}
+
+}
